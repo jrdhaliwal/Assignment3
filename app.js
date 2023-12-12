081ea0b7f5f730272a1201e439aa025cc32c2a29
jrdhaliwal <146687083+jrdhaliwal@users.noreply.github.com>
2023-12-11 22:09:21 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path");
 const {returnArtists} = require("./dataProvider");
 const {returnArtistsByCountry} = require("./dataProvider");
+const {returnGalleries} = require("./dataProvider");
+const {returnGalleriesByCountry} = require("./dataProvider");
+const {returnPaintings} = require("./dataProvider");
+const {returnPaintingById} = require("./dataProvider");
+const {returnPaintingsByGalleryId} = require("./dataProvider");
+const {returnPaintingsByArtistId} = require("./dataProvider");
+const {returnPaintingByText} = require("./dataProvider");
+const {returnPaintingsByYear} = require("./dataProvider");
+const {returnPaintingByColor} = require("./dataProvider");
 
 const app = express();
 const port = 3000;
@@ -21,7 +30,100 @@ app.get("/api/artists", (req, res) => {
 app.get("/api/artists/:country", (req, res) => {
     const country = req.params.country;
     const artists = returnArtistsByCountry(country);
+
+    if (artists.length == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
     res.json(artists);
+})
+
+app.get("/api/galleries", (req, res) => {
+    const galleries = returnGalleries();
+    res.json(galleries);
+})
+
+app.get("/api/galleries/:country", (req, res) => {
+    const country = req.params.country;
+    const galleries = returnGalleriesByCountry(country);
+
+    if (galleries.length == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
+    res.json(galleries);
+})
+
+app.get("/api/paintings", (req, res) => {
+    const paintings = returnPaintings();
+    res.json(paintings);
+})
+
+app.get("/api/painting/:id", (req, res) => {
+    const id = req.params.id;
+    const paintings = returnPaintingById(id);
+
+    if (paintings.length == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
+    res.json(paintings);
+})
+
+app.get("/api/painting/gallery/:id", (req, res) => {
+    const id = req.params.id;
+    const paintings = returnPaintingsByGalleryId(id);
+
+    if (paintings == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
+    res.json(paintings);
+})
+
+app.get("/api/painting/artist/:id", (req, res) => {
+    const id = req.params.id;
+    const paintings = returnPaintingsByArtistId(id);
+
+    if (paintings == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
+    res.json(paintings);
+})
+
+app.get("/api/painting/year/:min/:max", (req, res) => {
+    const min = req.params.min;
+    const max = req.params.max;
+    const paintings = returnPaintingsByYear(min, max);
+
+    if (paintings == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
+    res.json(paintings);
+})
+
+app.get("/api/painting/title/:text", (req, res) => {
+    const text = req.params.text;
+    const paintings = returnPaintingByText(text);
+
+    if (paintings == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
+    res.json(paintings);
+})
+
+app.get("/api/painting/color/:name", (req, res) => {
+    const name = decodeURIComponent(req.params.name);
+    const paintings = returnPaintingByColor(name);
+
+    if (paintings == 0) {
+        return res.status(404).json({ message: 'Paintings not found' });
+    }
+
+    res.json(paintings);
 })
 
 app.listen(port, () => {
--- dataProvider.js
+++ dataProvider.js
@@ -1,14 +1,34 @@
 const path = require("path");
 const fs = require("fs");
 
+// Path and data for artists.json
 const artistsPath = path.join(__dirname, "data", "artists.json");
 const artistsData = fs.readFileSync(artistsPath);
 const artists = JSON.parse(artistsData);
 
-function capitalizeFirstLetter(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+// Path and data for galleries.json
+const galleriesPath = path.join(__dirname, "data", "galleries.json");
+const galleriesData = fs.readFileSync(galleriesPath);
+const galleries = JSON.parse(galleriesData);
+
+// Path and data for paintings-nested.json
+const paintingsPath = path.join(__dirname, "data", "paintings-nested.json");
+const paintingsData = fs.readFileSync(paintingsPath);
+const paintings = JSON.parse(paintingsData);
+
+// Function that ensures given word is case insensitive
+function capitalizeFirstLetter(sentence) {
+    if (sentence && typeof sentence === 'string') {
+        return sentence
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+            .join(' ');
+    }
+    return sentence; // Return unchanged if it's not a valid string
 }
 
+
+// Functions for artists.json
 function returnArtists() {
     return artists;
 }
@@ -19,4 +39,77 @@ function returnArtistsByCountry(country) {
     return found;
 }
 
-module.exports = {returnArtists, returnArtistsByCountry};
+// Functions for galleries.json
+function returnGalleries() {
+    return galleries;
+}
+
+function returnGalleriesByCountry(country) {
+    const c = capitalizeFirstLetter(country);
+    const found = galleries.filter(g => g.GalleryCountry == c);
+    return found;
+}
+
+// Functions for paintings-nested.json
+function returnPaintings() {
+    return paintings;
+}
+
+function returnPaintingById(id) {
+    const found = paintings.filter(p => p.paintingID == id);
+    return found;
+}
+
+function returnPaintingsByGalleryId(id) {
+    const found = paintings.filter(p => p.gallery.galleryID == id);
+    return found;
+}
+
+function returnPaintingsByArtistId(id) {
+    const found = paintings.filter(p => p.artist.artistID == id);
+    return found;
+}
+
+function returnPaintingsByYear(min, max) {
+    const found = paintings.filter(p => p.yearOfWork >= min && p.yearOfWork <= max);
+    return found;
+}
+
+function returnPaintingByText(word) {
+    const found = paintings.filter(p => {
+        const wordLower = word.toLowerCase();
+        const titleLower = p.title.toLowerCase();
+        return titleLower.includes(wordLower);
+    });
+
+    return found;
+}
+
+function returnPaintingByColor(color) {
+    const c = capitalizeFirstLetter(color);
+
+    const found = paintings.filter(p => {
+        for (let i = 0; i < p.details.annotation.dominantColors.length; i++) {
+            if (p.details.annotation.dominantColors[i].name == c) {
+                return p.details.annotation.dominantColors[i].name;
+            }
+        }
+    });
+
+    return found;
+}
+
+
+module.exports = {
+    returnArtists, 
+    returnArtistsByCountry, 
+    returnGalleries, 
+    returnGalleriesByCountry,
+    returnPaintings,
+    returnPaintingById,
+    returnPaintingsByGalleryId,
+    returnPaintingsByArtistId,
+    returnPaintingByText,
+    returnPaintingsByYear,
+    returnPaintingByColor
+};
